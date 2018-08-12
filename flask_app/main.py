b63fa2c9754a5c31ebd6856040f05a72f3f6d276
Kenny Rogers <kennyr87@gmail.com>
2018-08-12 12:21:47 -0400

    Change location of settings.py

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,9 +1,7 @@
-from application import (
-    create_app,
-    settings
-)
+from application import create_app
+from flask_app import settings
 
-app = create_app('application.settings.ProdConfig')
+app = create_app('settings.ProdConfig')
 
 if __name__ == "__main__":
     app.run()
