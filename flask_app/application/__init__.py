598f6ef200289d047bc4ccfefa721b1ddd23d6d6
Kenny Rogers <kennyr87@gmail.com>
2018-08-12 23:30:37 -0400

    Add support for flask-sqlalchemy

--- flask_app/application/__init__.py
+++ flask_app/application/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from application.controllers.main import main
 from settings import app_config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app(config_name):
 
     app = Flask('flask_app', template_folder='/srv/www/flask_app/templates')
+    # initialize configuration
     app.config.from_object(app_config[config_name])
-
+    # initialize database
+    db.init_app(app)
     # register our blueprints
     app.register_blueprint(main)
     return app
