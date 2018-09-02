f180778b60ec11ac52fceaa8c0d8f504f56190c4
Kenny Rogers <kennyr87@gmail.com>
2018-09-01 21:55:29 -0400

    Add socket to connection url string

--- flask_app/settings.py
+++ flask_app/settings.py
@@ -1,7 +1,11 @@
- # Flask configuration object
- # @see http://flask.pocoo.org/docs/1.0/config/#configuration-basics
+# Flask configuration object
+# @see http://flask.pocoo.org/docs/1.0/config/#configuration-basics
+#
+socket   = '?unix_socket=/var/run/mysqld/mysqld.sock'
+
 class Config(object):
     SECRET_KEY = 'REPLACE ME'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProdConfig(Config):
     ENV = 'prod'
@@ -12,7 +16,7 @@ class DevConfig(Config):
     ENV = 'dev'
     DEBUG = True
     EXPLAIN_TEMPLATE_LOADING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost/gigmicro'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost/gigmicro' + socket
 
     # CACHE_TYPE = 'null'
 
