9b75b47a57e093c46927a330ad84664f36930035
Kenny Rogers <kennyr87@gmail.com>
2018-09-01 21:54:28 -0400

    Fix errors in ORM classes

--- flask_app/application/models.py
+++ flask_app/application/models.py
@@ -1,19 +1,30 @@
-# from application import db
-# from sqlalchemy import db.Column, Index, ForeignKey, Table
-# from sqlalchemy.dialects.mysql import db.MEDIUMINT, db.VARCHAR, CHAR, DATE
+from application import db
+from sqlalchemy.dialects.mysql import MEDIUMINT
+
+# Association table for author to article bidirectional relationship
+author_article_assoc = db.Table('gigmc_author_article', db.Model.metadata,
+    db.Column('id', MEDIUMINT, primary_key=True),
+    db.Column('fk_author_id',
+        MEDIUMINT,
+        db.ForeignKey("gigmc_authors.author_id"),
+        nullable=False),
+    db.Column('fk_article_id',
+        MEDIUMINT,
+        db.ForeignKey("gigmc_articles.article_id"),
+        nullable=False)
+)
 
 # map classes to database tables
-
 class Journal(db.Model):
     __tablename__ = 'gigmc_journals'
 
-    jounrnal_id = db.Column(db.MEDIUMINT, primary_key=True)
+    journal_id = db.Column(MEDIUMINT, primary_key=True)
     journal_name = db.Column(db.VARCHAR(127), nullable=False)
     journal_url = db.Column(db.VARCHAR(255))
     issn_print = db.Column(db.CHAR(8), unique=True)
     issn_online = db.Column(db.CHAR(8), unique=True)
 
-    journal_articles = relationship("Article")
+    journal_articles = db.relationship("Article")
 
     def __repr__(self):
         return "<Journal( name = %s )>" % self.journal_name
@@ -21,11 +32,11 @@ class Journal(db.Model):
 class Article(db.Model):
     __tablename__ = 'gigmc_articles'
 
-    article_id = db.Column(db.MEDIUMINT, primary_key=True)
+    article_id = db.Column(MEDIUMINT, primary_key=True)
     article_name = db.Column(db.VARCHAR(127), nullable=False)
     publish_date = db.Column(db.DATE, nullable=False)
     fk_journal_id = db.Column(
-        db.MEDIUMINT,
+        MEDIUMINT,
         db.ForeignKey("gigmc_journals.journal_id"),
         nullable=False)
 
@@ -38,9 +49,9 @@ class Article(db.Model):
 class Author(db.Model):
     __tablename__ = 'gigmc_authors'
 
-    author_id = db.Column(db.MEDIUMINT, primary_key=True)
+    author_id = db.Column(MEDIUMINT, primary_key=True)
     first_name = db.Column(db.VARCHAR(31))
-    middle_initial = db.Column(CHAR(1))
+    middle_initial = db.Column(db.CHAR(1))
     last_name = db.Column(db.VARCHAR(31), nullable=False)
 
     __table_args__ = (
@@ -60,26 +71,13 @@ class Author(db.Model):
 class ArticleURL(db.Model):
     __tablename__ = 'gigmc_links'
 
-    link_id = db.Column(db.MEDIUMINT, primary_key=True)
+    link_id = db.Column(MEDIUMINT, primary_key=True)
     article_url = db.Column(db.VARCHAR(255), nullable=False)
     fk_article_id = db.Column(
-        db.MEDIUMINT,
+        MEDIUMINT,
         db.ForeignKey("gigmc_articles.article_id"),
         nullable=False)
 
     def __repr__(self):
         return "<URL(Link='%s', Name='%s')>" % (
             self.article_url, gigmc_articles.article_name)
-
-# Association table for author to article bidirectional relationship
-author_article_assoc = db.Table('gigmc_author_article', db.Model.metadata,
-    db.Column('id', db.MEDIUMINT, primary_key=True),
-    db.Column('fk_author_id',
-        db.MEDIUMINT,
-        db.ForeignKey("gigmc_authors.author_id"),
-        nullable=False),
-    db.Column('fk_article_id',
-        db.MEDIUMINT,
-        db.ForeignKey("gigmc_articles.article_id"),
-        nullable=False)
-)
