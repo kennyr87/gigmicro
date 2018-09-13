dc27cf95300e17f40c35d6cdf42930117b7de1ee
Kenny Rogers <kennyr87@gmail.com>
2018-09-13 09:48:14 -0400

    Refactor models

--- flask_app/application/models.py
+++ flask_app/application/models.py
@@ -2,7 +2,7 @@ from application import db
 from sqlalchemy.dialects.mysql import MEDIUMINT
 
 # Association table for author to article bidirectional relationship
-author_article_assoc = db.Table('gigmc_author_article', db.Model.metadata,
+author_article_assoc    = db.Table('gigmc_author_article', db.Model.metadata,
     db.Column('id', MEDIUMINT, primary_key=True),
     db.Column('fk_author_id',
         MEDIUMINT,
@@ -16,64 +16,64 @@ author_article_assoc = db.Table('gigmc_author_article', db.Model.metadata,
 
 # map classes to database tables
 class Journal(db.Model):
-    __tablename__ = 'gigmc_journals'
+    __tablename__       = 'gigmc_journals'
 
-    journal_id = db.Column(MEDIUMINT, primary_key=True)
-    journal_name = db.Column(db.VARCHAR(127), nullable=False)
-    journal_url = db.Column(db.VARCHAR(255))
-    issn_print = db.Column(db.CHAR(8), unique=True)
-    issn_online = db.Column(db.CHAR(8), unique=True)
+    journal_id          = db.Column(MEDIUMINT, primary_key=True)
+    journal_name        = db.Column(db.VARCHAR(127), nullable=False)
+    journal_url         = db.Column(db.VARCHAR(255))
+    issn_print          = db.Column(db.CHAR(8), unique=True)
+    issn_online         = db.Column(db.CHAR(8), unique=True)
 
-    journal_articles = db.relationship("Article")
+    journal_articles    = db.relationship("Article")
 
     def __repr__(self):
         return "<Journal( name = %s )>" % self.journal_name
 
 class Article(db.Model):
-    __tablename__ = 'gigmc_articles'
+    __tablename__   = 'gigmc_articles'
 
-    article_id = db.Column(MEDIUMINT, primary_key=True)
-    article_name = db.Column(db.VARCHAR(127), nullable=False)
-    publish_date = db.Column(db.DATE, nullable=False)
-    fk_journal_id = db.Column(
+    article_id      = db.Column(MEDIUMINT, primary_key=True)
+    article_name    = db.Column(db.VARCHAR(127), nullable=False)
+    publish_date    = db.Column(db.DATE, nullable=False)
+    fk_journal_id   = db.Column(
         MEDIUMINT,
         db.ForeignKey("gigmc_journals.journal_id"),
         nullable=False)
 
-    article_links = db.relationship("ArticleURL")
+    article_links   = db.relationship("ArticleURL")
 
     def __repr__(self):
         return "<Article(Title = %s)>" % (
             self.article_name)
 
 class Author(db.Model):
-    __tablename__ = 'gigmc_authors'
+    __tablename__   = 'gigmc_authors'
 
-    author_id = db.Column(MEDIUMINT, primary_key=True)
-    first_name = db.Column(db.VARCHAR(31))
-    middle_initial = db.Column(db.CHAR(1))
-    last_name = db.Column(db.VARCHAR(31), nullable=False)
+    author_id       = db.Column(MEDIUMINT, primary_key=True)
+    first_name      = db.Column(db.VARCHAR(31))
+    middle_initial  = db.Column(db.CHAR(1))
+    last_name       = db.Column(db.VARCHAR(31), nullable=False)
 
-    __table_args__ = (
+    __table_args__  = (
         db.Index('idx_lf_name', 'last_name', 'first_name'),
         )
 
-    articles = db.relationship(
+    articles            = db.relationship(
         "Article",
-        secondary=author_article_assoc,
-        backref="authors",
-        passive_deletes=True)
+        secondary       = author_article_assoc,
+        backref         = "authors",
+        passive_deletes = True)
 
     def __repr__(self):
         return "<Author(Full Name=%s %s)>" % (
             self.first_name, self.last_name)
 
 class ArticleURL(db.Model):
-    __tablename__ = 'gigmc_links'
+    __tablename__   = 'gigmc_links'
 
-    link_id = db.Column(MEDIUMINT, primary_key=True)
-    article_url = db.Column(db.VARCHAR(255), nullable=False)
-    fk_article_id = db.Column(
+    link_id         = db.Column(MEDIUMINT, primary_key=True)
+    article_url     = db.Column(db.VARCHAR(255), nullable=False)
+    fk_article_id   = db.Column(
         MEDIUMINT,
         db.ForeignKey("gigmc_articles.article_id"),
         nullable=False)
