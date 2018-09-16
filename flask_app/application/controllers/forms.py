93dab2060cafce09a7d059ba1fa7479bb1439773
Kenny Rogers <kennyr87@gmail.com>
2018-09-16 00:59:10 -0400

    Add view to render form for journal

---
+++ flask_app/application/controllers/forms.py
@@ -0,0 +1,10 @@
+
+from flask import Blueprint, render_template
+from application.forms import JournalForm
+
+bp      = Blueprint('forms', __name__, url_prefix='/forms')
+
+@bp.route('/journal')
+def journal_form():
+    form    = JournalForm()
+    return render_template('journal.html', form=form)
