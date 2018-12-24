b7a3d0abfa4226fcfc7f0ecd0c4e1d45ba9a4037
Kenny Rogers <kennyr87@gmail.com>
2018-12-23 20:27:41 -0500

    Correct syntax errors in AppTestSuite

--- flask_app/tests/__init__.py
+++ flask_app/tests/__init__.py
@@ -4,17 +4,17 @@
     tests package
 """
 from unittest import TestSuite, TextTestRunner
-from tests.journal_tests import JournalTestCase
+from .journal_tests import JournalTestCase
 
 class AppTestSuite(TestSuite):
 
-    test_cases = (JournalTestCase())
+    test_cases = JournalTestCase()
 
     def __init__(self):
-        super(TestSuite, self).__init__()
-        self.addTest(test_cases)
+        super(AppTestSuite, self).__init__()
+        self.addTest(self.test_cases)
 
 if __name__ == '__main__':
     suite = AppTestSuite()
-    runner = unittest.TextTestRunner()
+    runner = TextTestRunner()
     runner.run(suite)
