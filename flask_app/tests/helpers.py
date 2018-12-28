e83c40aca4da1600c058e57ee838a08b9e159c3e
Kenny Rogers <kennyr87@gmail.com>
2018-12-28 12:00:21 -0500

    Update documentation for methods

--- flask_app/tests/helpers.py
+++ flask_app/tests/helpers.py
@@ -16,10 +16,13 @@ class FlaskTestCaseMixin(object):
 
     Attributes:
         csrf_token (str): Flask generated CSRF token.
-        client (:obj: ``): Flask test client
+        client (str): Werkzeug test client
     """
 
     def _create_csrf_token(self):
+        """Creates CSRF token.
+        """
+
         csrf_key = 'csrf_token'
         with self.client.session_transaction() as session:
             session['csrf'] = csrf_key
@@ -40,7 +43,7 @@ class FlaskTestCaseMixin(object):
         return kwargs
 
     def _json_data(self, csrf_enabled=True, **kwargs):
-        """HTTP headers for JSON data
+        """HTTP headers to send JSON data
         """
         if 'data' in kwargs:
             kwargs['data']['csrf_token'] = self.csrf_token
@@ -93,81 +96,115 @@ class FlaskTestCaseMixin(object):
 
     def assertStatusCode(self, response, status_code):
         """Assert the status code of a Flask test client response
-        :param response: The test client response object
-        :param status_code: The expected status code
+
+        Args:
+            response: The test client response object
+            status_code: The expected status code
+
         """
         self.assertEquals(status_code, response.status_code)
         return response
 
     def assertOk(self, response):
         """Test that response status code is 200
-        :param response: The test client response object
+
+        Args:
+            response: The test client response object
+
         """
         return self.assertStatusCode(response, 200)
 
     def assertBadRequest(self, response):
         """Test that response status code is 400
-        :param response: The test client response object
+
+        Args:
+            response: The test client response object
+
         """
         return self.assertStatusCode(response, 400)
 
     def assertForbidden(self, response):
         """Test that response status code is 403
-        :param response: The test client response object
+
+        Args:
+            response: The test client response object
+
         """
         return self.assertStatusCode(response, 403)
 
     def assertNotFound(self, response):
         """Test that response status code is 404
-        :param response: The test client response object
+
+        Args:
+            response: The test client response object
+
         """
         return self.assertStatusCode(response, 404)
 
     def assertContentType(self, response, content_type):
         """Assert the content-type of a Flask test client response
-        :param response: The test client response object
-        :param content_type: The expected content type
+
+        Args:
+            response: The test client response object
+            content_type: The expected content type
+
         """
         self.assertEqual(content_type, response.headers['Content-Type'])
         return response
 
     def assertOkHtml(self, response):
         """Assert the response status code is 200 and an HTML response
-        :param response: The test client response object
+
+        Args:
+            response: The test client response object
+
         """
         return self.assertOk(
             self.assertContentType(response, 'text/html; charset=utf-8'))
 
     def assertJson(self, response):
         """Test that content returned is in JSON format
-        :param response: The test client response object
+
+        Args:
+            response: The test client response object
         """
         return self.assertContentType(response, 'application/json')
 
     def assertOkJson(self, response):
         """Assert the response status code is 200 and a JSON response
-        :param response: The test client response object
+
+        Args:
+            response: The test client response object
+
         """
         return self.assertOk(self.assertJson(response))
 
     def assertBadJson(self, response):
         """Assert the response status code is 400 and a JSON response
-        :param response: The test client response object
+
+        Args:
+            response: The test client response object
+
         """
         return self.assertBadRequest(self.assertJson(response))
 
     def assertCookie(self, response, name):
         """Assert the response contains a cookie with the specified name
-        :param response: The test client response object
-        :param key: The cookie name
-        :param value: The value of the cookie
+
+        Args:
+            response: The test client response object
+            name: The cookie name
+
         """
         self.assertIn(name, self.getCookies(response))
 
     def assertCookieEquals(self, response, name, value):
         """Assert the response contains a cookie with the specified value
-        :param response: The test client response object
-        :param name: The cookie name
-        :param value: The value of the cookie
+
+        Args:
+            response: The test client response object
+            name: The cookie name
+            value: The value of the cookie
+
         """
         self.assertEquals(value, self.getCookies(response).get(name, None))
