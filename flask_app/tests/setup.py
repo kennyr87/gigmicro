5d5cdd7cf97eaa4e2c80ee07c6d96c32c395bab7
Kenny Rogers <kennyr87@gmail.com>
2018-12-31 17:10:55 -0500

    Configure tests to use and clear scoped session
    - Subclass TextTestRunner to configure session on start
    - Configure AppTestCase to setup and teardown scoped session

--- flask_app/tests/setup.py
+++ flask_app/tests/setup.py
@@ -1,11 +1,27 @@
+# -*- coding: utf-8 -*-
 """
     tests.setup
     ~~~~~
     test setup environment
 """
 from application import create_app
-from unittest import TestCase
-from .helpers import FlaskTestCaseMixin
+from unittest import TestCase, TextTestRunner
+from tests.helpers import FlaskTestCaseMixin
+from tests import Session
+from tests.settings import SQLALCHEMY_DATABASE_URI
+from sqlalchemy import create_engine
+
+class AppTestRunner(TextTestRunner):
+    """Basic text test runner implementation.
+
+    """
+    def __init__(self):
+        """Configure SQLAlchemy session on start.
+
+        """
+        super(AppTestRunner, self).__init__()
+        engine = create_engine(SQLALCHEMY_DATABASE_URI)
+        Session.configure(bind=engine)
 
 class AppTestCase(FlaskTestCaseMixin, TestCase):
     """Create shared fixtures for all test cases
@@ -22,6 +38,8 @@ class AppTestCase(FlaskTestCaseMixin, TestCase):
 
     def setUp(self):
         super(AppTestCase, self).setUp()
+
+        # setup Flask request context
         self.app = self._create_app()
         self.client = self.app.test_client()
         self.app_context = self.app.app_context()
@@ -29,6 +47,15 @@ class AppTestCase(FlaskTestCaseMixin, TestCase):
         self._create_fixtures()
         self._create_csrf_token()
 
+        # setup SQL Alchemy session for factories
+        self.session = Session()
+
     def tearDown(self):
         super(AppTestCase, self).tearDown()
+
+        # remove Flask request context
         self.app_context.pop()
+
+        # remove SQL Alchemy session
+         self.session.rollback()
+         self.session.close()
