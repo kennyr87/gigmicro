2db857872e9e6bbafeb7eda388128832b95bf6a7
Kenny Rogers <kennyr87@gmail.com>
2019-01-03 14:42:49 -0500

    Fix error with adding correct attributes to factory classes

--- flask_app/tests/factory.py
+++ flask_app/tests/factory.py
@@ -8,17 +8,17 @@
 
 from factory import SubFactory, Faker, alchemy, Sequence, LazyAttribute
 from application.models import Journal, Article, Author, ArticleURL
-from tests import Session
+from tests.setup import Session
 
 class JournalFactory(alchemy.SQLAlchemyModelFactory):
     class Meta:
         model               = Journal
         sqlalchemy_session  = Session
 
-        journal_name    = Faker('bs')
-        journal_url     = Faker('url')
-        issn_print      = Faker('ean', length=8)
-        issn_online     = Faker('ean', length=8)
+    journal_name    = Faker('bs')
+    journal_url     = Faker('url')
+    issn_print      = Faker('ean', length=8)
+    issn_online     = Faker('ean', length=8)
 
 
 class ArticleFactory(alchemy.SQLAlchemyModelFactory):
@@ -26,6 +26,6 @@ class ArticleFactory(alchemy.SQLAlchemyModelFactory):
         model               = Article
         sqlalchemy_session  = Session
 
-        article_name    = Faker('text', max_nb_chars=127)
-        publish_date    = Faker('data', pattern="%Y-%m-%d")
-        journal         = SubFactory(JournalFactory)
+    article_name    = Faker('text', max_nb_chars=127)
+    publish_date    = Faker('data', pattern="%Y-%m-%d")
+    journal         = SubFactory(JournalFactory)
