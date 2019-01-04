e9b9807c799d0ab8eb97b5863462ec4d100c7367
Kenny Rogers <kennyr87@gmail.com>
2019-01-03 19:10:51 -0500

    Create run_tests method to configure test runner

--- flask_app/tests/__init__.py
+++ flask_app/tests/__init__.py
@@ -6,17 +6,25 @@
 """
 from unittest import TestSuite, TextTestRunner, defaultTestLoader
 from tests.api.journal_tests import JournalTestCase
+from sqlalchemy import create_engine
+from tests.settings import SESSION, SQLALCHEMY_DATABASE_URI 
 
 class AppTestSuite(TestSuite):
 
-    test_cases = JournalTestCase()
+    test_suite = defaultTestLoader.loadTestsFromTestCase(JournalTestCase)
 
     def __init__(self):
         super(AppTestSuite, self).__init__()
-        self.addTest(self.test_cases)
+        self.addTest(self.test_suite)
 
-# need to add code to configure scoped_session
-if __name__ == '__main__':
+def run_tests():
+    # configure db connection
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    SESSION.configure(bind=engine)
+
     suite = AppTestSuite()
-    runner = AppTestRunner()
+    runner = TextTestRunner()
     runner.run(suite)
+
+if __name__ == '__main__':
+    run_tests()
