c339fc76cb8203dedc0ab6fea0641be1a0ef22b2
Kenny Rogers <kennyr87@gmail.com>
2019-01-03 19:13:10 -0500

    Move session global to settings.py

--- flask_app/tests/settings.py
+++ flask_app/tests/settings.py
@@ -5,6 +5,8 @@
 
     tests settings module
 """
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 SOCKET = '?unix_socket=/var/run/mysqld/mysqld.sock' 
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost/gigmicro' + SOCKET
+SESSION = scoped_session(sessionmaker())
