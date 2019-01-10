d9185f2c3c3d8b4004eec880bfdaa34e3b8550b5
Kenny Rogers <kennyr87@gmail.com>
2019-01-10 13:36:37 -0500

    Add test to create journal from JSON request

--- flask_app/tests/api/journal_tests.py
+++ flask_app/tests/api/journal_tests.py
@@ -1,9 +1,10 @@
 """
-    tests.journal_tests
+    tests.api.journal_tests
     ~~~~~~~~~~~~~~~~~~~~~~~
     journal tests module
 """
 
+from unittest import skip
 from tests.setup import AppTestCase
 from tests.factory import JournalFactory, ArticleFactory
 
@@ -19,10 +20,21 @@ class JournalTestCase(AppTestCase):
         super(JournalTestCase, self)._create_fixtures()
         self.journal = JournalFactory()
 
+    @skip('skip test get journals')
     def test_get_journals(self):
         r = self.jget('/journals')
         self.assertOkJson(r)
 
+    @skip('skip test get journal')
     def test_get_journal(self):
         r = self.jget('/journals/%s' % self.journal.journal_id)
+        self.assertOkJson(r)
+
+    def test_create_journal(self):
+        r = self.jpost('/journals', data={
+            'journal_name': self.journal.journal_name,
+            'journal_url': self.journal.journal_url,
+            'issn_print': self.journal.issn_print,
+            'issn_online': self.journal.issn_online
+        })
         self.assertOkJson(r)
