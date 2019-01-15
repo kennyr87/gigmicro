d3383298c22d8a2db979aa62a86e17f945dc3e34
Kenny Rogers <kennyr87@gmail.com>
2019-01-14 19:23:50 -0500

    Delete application factory from package

--- flask_app/application/__init__.py
+++ flask_app/application/__init__.py
@@ -1,28 +1,6 @@
+# -*- coding: utf-8 -*-
 """
-    application
+    flask_app.application
     ~~~~~
     main application package
 """
-from flask import Flask, render_template
-from .api import main, journals
-from ..settings import app_config
-from .models import db
-from .helpers import JSONEncoder
-
-def on_404(error):
-    return render_template('errors/404.html', error=error), 404
-
-def create_app(config_name):
-
-    app = Flask('flask_app', template_folder='/srv/www/flask_app/templates')
-    # initialize configuration
-    app.config.from_object(app_config[config_name])
-    # initialize database
-    db.init_app(app)
-    # Set the default JSON encoder
-    app.json_encoder = JSONEncoder
-    # register our blueprints
-    app.register_blueprint(main.bp)
-    app.register_blueprint(journals.bp)
-    app.register_error_handler(404, on_404)
-    return app
